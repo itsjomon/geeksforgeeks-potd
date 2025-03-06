e1cab34b27365f79966d13d67ba031c0b5a59c45
Jomon Joy <202098873+itsjomon@users.noreply.github.com>
2025-03-06 17:39:00 +0530

    Subarrays with sum K

---
+++ january2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countSubarrays(int[] arr, int k) {
+        Map<Integer, Integer> prefixSumCount = new HashMap<>();
+        prefixSumCount.put(0, 1);
+        int sum = 0, count = 0;
+
+        for (int num : arr) {
+            sum += num;
+            count += prefixSumCount.getOrDefault(sum - k, 0);
+            prefixSumCount.put(sum, prefixSumCount.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+}
