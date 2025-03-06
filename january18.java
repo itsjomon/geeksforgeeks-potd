75c5d265f7da7072cae640825cc76de91c0622fd
Jomon Joy <202098873+itsjomon@users.noreply.github.com>
2025-03-06 17:54:54 +0530

    Reverse a linked list

---
+++ january18.java
@@ -0,0 +1,12 @@
+class Solution {
+    Node reverseList(Node head) {
+        Node prev = null;
+        while (head != null) {
+            Node next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
